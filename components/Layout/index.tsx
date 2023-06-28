cdc1bd269cdc63df7d170c5a0337cae0e78ec41d
Sergio Pinzon <sergio.pinzon@appxcale.com>
2023-06-28 16:52:45 -0500

    Highlights feature
    Highlights feature and fixes

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,16 +1,23 @@
-import Link from "next/link"
-import CenteredTabs from "../tabs/CenteredTabs"
-import TopMenu from "../topMenu"
+import Link from "next/link";
+import CenteredTabs from "../tabs/CenteredTabs";
+import TopMenu from "../topMenu";
+import { Grid } from "@mui/material";
+import Trending from "../trending/Trending";
 
-export default function Layout({children}:React.PropsWithChildren) {
-
+export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <>
-    <TopMenu></TopMenu>
-    <div className="bg-gray-100 min-h-screen flex h-11/12">
-      <div className="bg-white flex-grow mt-1 mr-2 rounded-lg p-4">{children}<br/>
-      </div>
-    </div>
+      <TopMenu />
+      <Grid container sx={{ bgcolor: "gray.100", minHeight: "100vh" }}>
+        <Grid item xs={8}>
+          <div className="bg-white rounded-lg p-4">
+            {children}
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <Trending/>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xsgames.co',
+        port: '',
+        pathname: '/randomusers/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
